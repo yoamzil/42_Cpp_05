543296a22bff5fb69bccd3dfd0c5457a4dab48b8
Youness Amzil <amzilyouness@gmail.com>
2023-11-25 16:48:16 +0100

    Getting started with the first exercice.

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************************** */
+/*																						  */
+/*                                                						d8b		888       */
+/*                                                  					Y8P		888       */
+/*                                                      						888       */
+/*		888  888	  .d88b.   	8888b.  	88888b.d88b.	88888888	888 	888       */
+/*		888  888 	d88""88b     	"88b 	888 "888 "88b    	d88P 	888 	888       */
+/*		888  888 	888  888 	.d888888 	888  888  888		d88P   	888 	888       */
+/*		Y88b 888 	Y88..88P 	888  888 	888  888  888	  d88P    	888 	888       */
+/*		"Y88888  	"Y88P"  	"Y888888 	888  888  888	88888888 	888 	888       */
+/*	  		 888																          */
+/*		Y8b d88P																          */
+/*		"Y88P"									By: yoamzil <yoamzil@student.1337.ma>     */
+/*																						  */
+/* ************************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+    std::cout << "Bureaucrat constructor called" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Bureaucrat destructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &original)
+{
+    std::cout << "Bureaucrat copy constructor called" << std::endl;
+    *this = original;
+}
+
+Bureaucrat  &Bureaucrat::operator=(Bureaucrat const &original)
+{
+    if (this != &original)
+    {
+        // this->type = original.type;
+    }
+    return (*this);
+}
